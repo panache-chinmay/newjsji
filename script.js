303ee5ceddc7528516181cbd04dd0492703e0324
panache-chinmay <chinmaydeshpande010@gmail.com>
2021-06-21 11:06:47 +0530

    newCode

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+let a = 10
+console.log(a)
+console.log(typeof a)
+
+
+
+let firstName = "chinmay"
+console.log(typeof firstName)
+
+// Number ,Strig ,Boolean ,Array ,Objects
+// arithmetic operations 
+
+let x = 10
+let y = 20 
+
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+
+let u = 30
+let r  = 50
+
+console.log(u+r)
+console.log(u-r)
+console.log(u*r)
+console.log(u/r)
+
+// Function 
+//function declartion
+// keyword   functionname  paramters
+function cal(x,y){   // blocks 
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)  // statements
+}
+
+cal(50,25)   // function call  (actual values ) arguments 
+cal(50,40)
+cal(100,50)
+
+
+
+// DRY -- Donot repeat yourself 
+
+
+
+
+
+
+
+
+
+
+
+
+
